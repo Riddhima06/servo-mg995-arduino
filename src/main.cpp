f6cca6c96bcacad18e3e70488e0bc359592f3d1c
Riddhima06 <riddhima.rajput_cs.aiml24@gla.ac.in>
2026-02-04 13:42:31 +0530

    Initialise pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
+#include <Servo.h>
+Servo myServo;
 void setup() {
+    Serial.begin(9600);
+    myServo.attach(10);
+    Serial.println("Servo initiate");
+
 // write your initialization code here
 }
 
 void loop() {
+    myServo.write(0);
+    delay(500);
+    myServo.write(90);
+    delay(500);
+    myServo.write(120);
+    delay(500);
+    myServo.write(180);
+    delay(500);
 // write your code here
 }
